41ad3e5606f18706ac16e692703ac4289f0f1b67
saru <sarguments@gmail.com>
2020-07-22 21:09:01 +0900

    기존 지뢰인덱스에 존재하는지를 중복 체크 하지 않게 수정

--- src/main/java/me/saru/MineManager.java
+++ src/main/java/me/saru/MineManager.java
@@ -30,24 +30,28 @@ public class MineManager {
         strategies.add(new RDCalculateStrategy(rowCount));
     }
 
-    // 허용 범위 안에서 지뢰 카운트를 증가시킬 박스 인덱스를 구한다. 해당 인덱스에 지뢰가 이미 있는 경우는 제외한다.
+    // 지뢰 카운트를 증가시킬 박스 인덱스를 구한다.
     public List<Integer> calcMineNumbers() {
         List<Integer> result = new ArrayList<>();
 
         // 리스트에 중복 숫자가 들어와도 해쉬셋 변환 시 중복 제거됨
         Set<Integer> collect = new HashSet<>(numbers);
+
+        // 지뢰 인덱스에 대해 '해당 전략으로 이동이 가능한지' 를 만족하는 전략들을 필터링 해서 증가시킬 박스 인덱스를 구한다.
         collect.forEach(
                 mineNumber -> strategies.stream().filter(
-                        strategy -> strategy.isPassable(mineNumber) && !numbers.contains(strategy.getCalculateNumber(mineNumber))
+                        strategy -> strategy.isPassable(mineNumber)
                 ).forEach(filteredStrategy -> calculate(filteredStrategy, mineNumber, result)));
 
         return result;
     }
 
-    // 선택된 로직에 유효한 경우 그에 해당하는 박스 인덱스를 결과 리스트에 추가한다.
+    // 선택된 로직에 유효한 경우 그에 해당하는 박스 인덱스를 결과 리스트에 추가한다. 해당 인덱스에 지뢰가 이미 있는 경우는 제외한다.
     public void calculate(CalculateStrategy strategy, int mineNumber, List<Integer> result) {
-        if (strategy.isPassable(mineNumber)) {
-            result.add(strategy.getCalculateNumber(mineNumber));
+        int calculateNumber = strategy.getCalculateNumber(mineNumber);
+
+        if (strategy.isPassable(mineNumber) && !numbers.contains(calculateNumber)) {
+            result.add(calculateNumber);
         }
     }
 
